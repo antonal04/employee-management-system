512c1bd3189e4fc2802a7922012a79281cbc2ea6
Anton <anton.aloy@althisolutions.com>
2026-09-24 11:43:33 +0530

    first commit

---
+++ ems-application/src/main/java/com/example/EmsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.example")
+public class EmsApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EmsApplication.class,args);
+    }
+}
