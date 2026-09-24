6f2f9bfe79878b11fda643158574d8300c62a859
Anton <anton.aloy@althisolutions.com>
2026-09-24 12:06:00 +0530

    new

--- ems-application/src/main/java/com/example/EmsApplication.java
+++ ems-application/src/main/java/com/example/EmsApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.example")
 public class EmsApplication {
+    //gbbofughogboghi
     public static void main(String[] args)
     {
         SpringApplication.run(EmsApplication.class,args);
