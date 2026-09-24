5a26ae0c71666b28604b08452affaf2bfcb5176d
Anton <anton.aloy@althisolutions.com>
2026-09-24 12:08:04 +0530

    Add employee management features

--- ems-application/src/main/java/com/example/EmsApplication.java
+++ ems-application/src/main/java/com/example/EmsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.example")
 public class EmsApplication {
-    //gbbofughogboghi
+    //gbbofugho
     public static void main(String[] args)
     {
         SpringApplication.run(EmsApplication.class,args);
